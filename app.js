1c7c4e5d48c06493c08ce1f1e263eb06a7307880
TrentTucker <dutyforthebooty@gmail.com>
2021-02-24 14:55:19 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const dotenv = require('dotenv')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+dotenv.config()
+
+client.on('ready', () => {
+    console.log('What if I was online? 😳');
+  });
+
+client.on('message', msg => {
+    msg.react('😳')
+})
+
+client.login(process.env.TOKEN)
